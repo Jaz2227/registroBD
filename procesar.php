47e05e7f9e3d5d3b58995492cbc19bcecee047f2
Jaz2227 <chiva368@hotmail.com>
2014-03-26 14:43:27 -0600

    practica empleando en el celular

---
+++ procesar.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+require'conecta.php';
+
+
+$nombre=$_POST["Nom"];
+
+$mail=$_POST["Mail"];
+
+$telefono=$_POST["Telef"];
+
+
+$sql="INSERT INTO clientes(id_clientes, nombre, mail, telefono)VALUES(' ','$nombre','$mail','$telefono')";
+
+$resultado=mysql_query($sql,$con);
+
+
+if($resultado){
+ echo "El registro se almaceno de manera correcta: ".$nombre." Mail: ".$mail." Telefono: ".$telefono;       
+}else{
+ die("Error de conexión".mysql_error());   
+
+}
+
+mysql_close($con);
+
+?>
